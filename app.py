d848b3376fd698dcca0658b5b7167f6f91bc7ea3
yash25hemnani <yashhemnanigit@gmail.com>
2025-04-18 22:17:24 +0530

    Fixed CORS

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ def create_app():
     app = Flask(__name__)
     register_routes(app)
     
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app,
+        resources={r"/api/*": {"origins": ["https://next-finance-tracker-six.vercel.app"]}},
+        supports_credentials=True,
+        methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+        allow_headers=["Content-Type", "Authorization"]
+    )
 
     @app.route('/')
     def index():
