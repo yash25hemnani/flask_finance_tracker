27da197f581fc87a06e60ede590080ad8e18975e
yash25hemnani <yashhemnanigit@gmail.com>
2025-04-18 20:03:09 +0530

    Fixed CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from routes import register_routes
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     register_routes(app)
+    CORS(app)
     return app
 
 if __name__ == '__main__':
